9f3c64b38433940478ba260f1d12d325a6c8d6d3
szabadiandras <szabadiandras93@gmail.com>
2019-09-12 13:19:40 +0200

    current progress added

---
+++ week-01/day-03/hello-others.ts
@@ -0,0 +1,6 @@
+ 'use strict';
+
+    //Greeting three different person
+    console.log('Hello Ester!');
+    console.log('Hello, Mary!');
+    console.log('Hello Joe!');
---
+++ week-01/day-03/introduce-yourself.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('Andras Szabadi');
+console.log(25);
+console.log(1.79);
