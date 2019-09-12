740af899ca8be4a9705a58ed70c11db241df5046
szabadiandras <szabadiandras93@gmail.com>
2019-09-12 17:34:55 +0200

    added fedine basic info

---
+++ week-01/day-03/define-basic-info.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = 'Andras Szabadi';
+console.log(myName);
+
+let myAge: number = 25;
+console.log(myAge);
+
+let myHeight: number = 1.78;
+console.log(myHeight);
+
+let myStatus: boolean = false;
+console.log(myStatus); 
