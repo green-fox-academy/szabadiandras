1c2be2a290b1db2f5bab499cebbff3fc2d1b9087
szabadiandras <szabadiandras93@gmail.com>
2019-09-12 13:41:02 +0200

    added two-numbers.ts

---
+++ week-01/day-03/two-numbers.ts
@@ -0,0 +1,36 @@
+'use strict';
+console.log("Operations on the numbers 22 and 13.");
+console.log("");
+
+let myNumber = 22;
+let mySecondNumber = 13;
+
+console.log("22+13");
+let addValue = myNumber + mySecondNumber;
+    console.log(addValue);
+
+console.log("");
+
+console.log("22-13");
+let subValue = myNumber - mySecondNumber;
+    console.log(subValue);
+
+console.log("");
+
+console.log("22*13")
+let multiValue = myNumber * mySecondNumber;
+    console.log(multiValue);
+
+console.log("")
+
+console.log("22/13")
+let divValue = myNumber / mySecondNumber;
+    console.log(divValue);
+
+console.log("")
+
+console.log("22%13")
+let remValue = myNumber % mySecondNumber;
+    console.log(remValue);
+
+
