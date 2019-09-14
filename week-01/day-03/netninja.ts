e847ac4651138f854d6fd8da121fbecade9cebfc
szabadiandras <szabadiandras93@gmail.com>
2019-09-15 01:43:18 +0200

    practice

--- week-01/day-03/netninja.ts
+++ week-01/day-03/netninja.ts
@@ -10,27 +10,49 @@ console.log(myLet3 + (' first'));
 myLet3 = 200;
 console.log(myLet3 + (' second'));
 
+console.log(' ');
+
 // var vs let
 
 const particle = {
-    x: 100, y: 100
+    x: 100, y: 200, z: 0
 };
 console.log(particle);
 
-particle.x = 200;
-console.log(particle.x);
-particle.x = particle.x -100;
-
+particle.z = 50;
+console.log(particle.z);
+particle.z = particle.z -25;
 console.log(particle);
-console.log(particle.x);
+console.log(particle.z);
 
 // particle.z = 50; <-- wont add z value to particle
 
-
+//the while loop
+console.log('\nThe while loop\n')
 
 let age: number = 5;
 while (age < 10) {
 	console.log("Your age is less than 10!");
 	age++;
 }
-document.write("You are now over 10!");
+    console.log("You are now over 10!");
+
+// the for loop
+console.log('\nThe for loop\n');
+
+let appleNumber: number = 0; // <- edit this number
+let appleMinNumber: number = 10;
+
+if (appleNumber === 0) {
+    console.log("You don't have any apple. Please take some.")
+    let appleNumberMore = (appleNumber + 1); 
+    for (appleNumberMore; appleNumberMore < appleMinNumber; appleNumberMore++) {
+    console.log('You have ' + (appleNumberMore) + ' apple(s) only. Take one more!');
+}
+        console.log('Now you have ' + appleMinNumber + ' apple(s)!');
+} else {
+    let appleNumberMore = (appleNumber + 0);
+    for (appleNumberMore; appleNumberMore < appleMinNumber; appleNumberMore++)
+    console.log('You have ' + (appleNumberMore) + ' apple(s) only. Take one more!');
+    console.log('Now you have ' + appleMinNumber + ' apple(s)!');
+}
