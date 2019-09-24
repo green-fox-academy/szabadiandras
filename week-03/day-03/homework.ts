46a2496046e6332a901a87d50293f3f6a896a074
szabadiandras <szabadiandras93@gmail.com>
2019-09-24 21:33:49 +0200

    homework for day 04

---
+++ week-03/day-03/homework.ts
@@ -0,0 +1,57 @@
+'use strict';
+
+// Homework for week-03/day-03
+
+// Create 5 trees
+// Store the data of them in variables in your program
+// for every tree the program should store its'
+// type
+// leaf color
+// age
+// sex
+// you can use just variables, or lists and/or maps
+
+let tree1: any = [
+    {
+        type: 'Planetree',
+        leafColor: 'light-green',
+        age: 50,
+        sex: 'male'
+    }
+]
+
+let tree2: any = [
+    {
+        type: 'Red Maple',
+        leafColor: 'red',
+        age: 75,
+        sex: 'male'
+    }
+]
+
+let tree3: any = [
+    {
+        type: 'Hackberry',
+        leafColor: 'yellow',
+        age: 75,
+        sex: 'female'
+    }
+]
+
+let tree4: any = [
+    {
+        type: 'Japanese Zelkova',
+        leafColor: 'green',
+        age: 100,
+        sex: 'male'
+    }
+]
+
+let tree5: any = [
+    {
+        type: 'Red Oak',
+        leafColor: 'dark-green',
+        age: 125,
+        sex: 'female'
+    }
+]
