a9231f4b4ecf90e894353f176c5f6cfb481f700b
szabadiandras <szabadiandras93@gmail.com>
2019-10-28 20:52:26 +0100

    SQL Exercises

--- week-06/day-04/greetings/app.js
+++ week-06/day-04/greetings/app.js
@@ -4,13 +4,16 @@ const app = express();
 
 app.use(express.static('../'));
 
+//e.g.: http://localhost:4000/?name=John
+//Welcome back, <%=name%>!
+//Welcome back, John!
+
 app.get('/', function (req, res) {
   if (req.query.name === undefined) {
-    res.render('home.ejs', {name: 'Guest'});
+    res.render('home.ejs', { name: 'Guest' });
   } else {
-    res.render('home.ejs', {name: req.query.name});
+    res.render('home.ejs', { name: req.query.name });
   }
-  
 });
 
 app.listen(PORT);
