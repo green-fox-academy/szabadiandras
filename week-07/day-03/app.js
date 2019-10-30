90cef2b3292785ff355e15b8521802334d632ea5
szabadiandras <szabadiandras93@gmail.com>
2019-10-30 15:32:16 +0100

    next task

--- week-07/day-03/app.js
+++ week-07/day-03/app.js
@@ -10,14 +10,30 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// DOUBLING
+
 app.get('/doubling', (req, res) => {
-  console.log("'/doubling' requested")
-  
-  let input = req.query.input
-  
-  res.send();
+  if (req.query.input !== isNaN) {
+    var input = {
+      "received": req.query.input,
+      "result": req.query.input * 2
+    }
+  } else if (req.query.input == isNaN) {
+    var input = {
+      "error": "Please provide an input!"
+    }
+  }
+
+  res.send(input);
+
 });
 
+// GREETER
+
+app.get('/greeter', (req, res) => {
+  let title = req.query.input;
+  res.send(title);
+});
 
 app.listen(PORT, () => {
   console.log(`\nServer is running.\nAccess at "localhost:${PORT}"`);
