3d162946a0d0b9240e9d189d77524a7f24cb51f4
szabadiandras <szabadiandras93@gmail.com>
2019-11-04 11:10:54 +0100

    upvote and downvote now working properly

--- week-07/day-04/reddit-backend/app.js
+++ week-07/day-04/reddit-backend/app.js
@@ -32,6 +32,9 @@ app.get('/hello', function (req, res) {
   res.send('Hello');
 });
 
+
+//GET POSTS
+
 app.get('/posts', (req, res) => {
   const query = "SELECT * FROM post"
   conn.query(query, (err, posts) => {
@@ -41,38 +44,66 @@ app.get('/posts', (req, res) => {
   });
 });
 
+
+//NEW POST
+
 app.post('/posts', function (req, res) {
-  const time = new Date().getTime();
-  const query = `INSERT INTO reddit.post (title, url, timestamp, score) VALUES ('${req.body.title}', 'localhost:8080/posts/${req.body.title}', '${time}', '${req.body.score}')`
+  const dateObj = new Date();
+  const date = `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
+  const query = `INSERT INTO reddit.post (title, url, timestamp, score) VALUES ('${req.body.title}', 'localhost:8080/posts/${req.body.title}', '${date}', '0')`
+  conn.query(query, (err, post) => {
+    console.log(err)
+    console.log(post);
+    const query = `SELECT * FROM post WHERE id=${post.insertId}`
+    conn.query(query, (err, post) => {
+      res.setHeader("Content-type", "application/json");
+      res.status(200);
+      res.send(JSON.stringify(post) + `\n${req.body.title} is added to posts.`);
+    })
+  });
+});
+
+
+//UPVOTE
+
+app.put('/posts/:id/upvote', function (req, res) {
+  const query = `UPDATE reddit.post SET score = score + 1 WHERE (id = ${req.params.id})`
   conn.query(query, (err, posts) => {
     console.log(err)
     res.setHeader("Content-type", "application/json");
     res.status(200);
-    res.send(JSON.stringify(query));
+    res.send(JSON.stringify(query) + `\nThe post "${req.body.title}" has been upvoted.`);
   });
 });
 
-app.put('/posts/:id/upvote', function (req, res) {
-  res.send(req.params.id);
-});
+
+//DOWNVOTE
 
 app.put('/posts/:id/downvote', function (req, res) {
-  res.send(req.params.id);
+  const query = `UPDATE reddit.post SET score = score - 1 WHERE (id = ${req.params.id})`
+  conn.query(query, (err, posts) => {
+    console.log(err)
+    res.setHeader("Content-type", "application/json");
+    res.status(200);
+    res.send(JSON.stringify(query) + `\nThe post "${req.body.title}" has been downvoted. Current score is ${req.body.score}`);
+  });
 });
 
+
 //DELETE (optional)
+
 app.delete('/posts/:id', function (req, res) {
-  const id = ':id'
-  const query = `DELETE FROM reddit.post WHERE (id = ${id})`
-  res.send(req.params.id);
+  const query = `DELETE FROM reddit.post WHERE id = ${req.params.id}`
   conn.query(query, (err, posts) => {
     res.setHeader("Content-type", "application/json");
     res.status(200);
-    res.send(JSON.stringify(query));
+    res.send(JSON.stringify(query) + '\n' + req.params.id + ' is deleted.');
   });
 });
 
+
 //PUT (optional)
+
 app.put('/posts/:id', function (req, res) {
   res.send(req.params.id);
 });
