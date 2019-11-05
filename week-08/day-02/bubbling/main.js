8e6e32b46960be660d4571fdcc0580bccf0e94ae
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 15:20:26 +0100

    minor changes

--- week-08/day-02/bubbling/main.js
+++ week-08/day-02/bubbling/main.js
@@ -1,25 +1,26 @@
 'use strict';
 
 const nav = document.querySelector('nav');
+const inspector = document.getElementsByClassName(".img-inspector");
 
 nav.addEventListener('click', function (event) {
   if (event.target.attributes[1].value === 'up') {
     console.log('Up');
-    document.getElementsByClassName(".img-inspector")
+    
   } else if (event.target.attributes[1].value === 'down') {
     console.log('Down');
-    document.getElementsByClassName(".img-inspector")
+    inspector
   } else if (event.target.attributes[1].value === 'left') {
     console.log('Left');
-    document.getElementsByClassName(".img-inspector")
+    inspector
   } else if (event.target.attributes[1].value === 'right') {
     console.log('Right');
-    document.getElementsByClassName(".img-inspector")
+    inspector
   } else if (event.target.attributes[1].value === 'in') {
     console.log('Zoom-in');
-    document.getElementsByClassName(".img-inspector")
+    inspector
   } else if (event.target.attributes[1].value === 'out') {
     console.log('Zoom-out');
-    document.getElementsByClassName(".img-inspector")
+    inspector
   }
 });
