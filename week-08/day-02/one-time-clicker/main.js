ca21b979f1bdb555ec9c4945211d0185de8606bd
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 13:34:08 +0100

    first solution to one-time-clicker added

--- week-08/day-02/one-time-clicker/main.js
+++ week-08/day-02/one-time-clicker/main.js
@@ -2,6 +2,14 @@
 
 let button = document.querySelector('button')
 
+//First solution
 button.addEventListener('click', function(){
   console.log(Date());
+  button.disabled = true;
+});
+
+//Second solution
+button.addEventListener('click', function(){
+  console.log(Date());
+  button.disabled = true;
 });
