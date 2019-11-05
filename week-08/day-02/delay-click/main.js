6375e6eeb79af017dc84f7be18dd3485d56700a5
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 17:06:39 +0100

    click three times task pre

--- week-08/day-02/delay-click/main.js
+++ week-08/day-02/delay-click/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Create a simple HTML document with one button. 
-// If the user clicks the button it should wait 2 seconds 
-// and it should show a text under the button: 2 seconds ellapsed
-
 const button = document.querySelector('button');
 const text = document.querySelector('p')
 
