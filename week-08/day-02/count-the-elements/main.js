37149b53c39de5345f7450a6e66681ee86c30832
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 12:49:18 +0100

    count-the-elements exercise

---
+++ week-08/day-02/count-the-elements/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const button = document.querySelector('#click');
+const listCounter = () => {
+  let li = document.querySelector('li');
+  for (let i = 0; i < li; i++) {
+    let result = 
+    
+  }
+};
+button.addEventListener('click', listCounter);
