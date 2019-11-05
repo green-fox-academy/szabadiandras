2b51dcf12724c7ed4e41ff88d825508b328361ea
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 15:41:01 +0100

    zoom in and out

--- week-08/day-02/bubbling/main.js
+++ week-08/day-02/bubbling/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
 const nav = document.querySelector('nav');
-const inspector = document.getElementsByClassName(".img-inspector");
+const inspector = document.querySelector('.img-inspector');
 
 nav.addEventListener('click', function (event) {
   if (event.target.attributes[1].value === 'up') {
     console.log('Up');
-    
+  
   } else if (event.target.attributes[1].value === 'down') {
     console.log('Down');
-    inspector
+  
   } else if (event.target.attributes[1].value === 'left') {
     console.log('Left');
-    inspector
+    
   } else if (event.target.attributes[1].value === 'right') {
     console.log('Right');
-    inspector
+    inspector.style.margin = "20"
   } else if (event.target.attributes[1].value === 'in') {
     console.log('Zoom-in');
-    inspector
+    inspector.style.backgroundSize = "300%"
   } else if (event.target.attributes[1].value === 'out') {
     console.log('Zoom-out');
-    inspector
+    inspector.style.backgroundSize = "100%"
   }
 });
