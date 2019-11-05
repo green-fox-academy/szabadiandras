2170ba18a76d84d07273ad04bf701b0242c4dc46
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 15:13:02 +0100

    functionality added to image inspector

--- week-08/day-02/bubbling/main.js
+++ week-08/day-02/bubbling/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
-let nav = document.querySelector('nav');
+const nav = document.querySelector('nav');
 
-nav.addEventListener('click', function(event){
-  console.log(nav.children);
+nav.addEventListener('click', function (event) {
+  if (event.target.attributes[1].value === 'up') {
+    console.log('Up');
+    document.getElementsByClassName(".img-inspector")
+  } else if (event.target.attributes[1].value === 'down') {
+    console.log('Down');
+    document.getElementsByClassName(".img-inspector")
+  } else if (event.target.attributes[1].value === 'left') {
+    console.log('Left');
+    document.getElementsByClassName(".img-inspector")
+  } else if (event.target.attributes[1].value === 'right') {
+    console.log('Right');
+    document.getElementsByClassName(".img-inspector")
+  } else if (event.target.attributes[1].value === 'in') {
+    console.log('Zoom-in');
+    document.getElementsByClassName(".img-inspector")
+  } else if (event.target.attributes[1].value === 'out') {
+    console.log('Zoom-out');
+    document.getElementsByClassName(".img-inspector")
+  }
 });
