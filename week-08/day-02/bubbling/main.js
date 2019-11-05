b16d137a4b28f428fa54eb875c78ac716afa73e0
szabadiandras <szabadiandras93@gmail.com>
2019-11-05 16:00:03 +0100

    update

--- week-08/day-02/bubbling/main.js
+++ week-08/day-02/bubbling/main.js
@@ -2,25 +2,34 @@
 
 const nav = document.querySelector('nav');
 const inspector = document.querySelector('.img-inspector');
+let zoom = 200;
 
 nav.addEventListener('click', function (event) {
+  
   if (event.target.attributes[1].value === 'up') {
+    inspector.style.backgroundSize = "300%"
     console.log('Up');
   
   } else if (event.target.attributes[1].value === 'down') {
+    inspector.style.backgroundSize = "300%"
     console.log('Down');
   
   } else if (event.target.attributes[1].value === 'left') {
-    console.log('Left');
-    
-  } else if (event.target.attributes[1].value === 'right') {
-    console.log('Right');
-    inspector.style.margin = "20"
-  } else if (event.target.attributes[1].value === 'in') {
-    console.log('Zoom-in');
     inspector.style.backgroundSize = "300%"
+    console.log('Left');
+  
+  } else if (event.target.attributes[1].value === 'right') {
+    inspector.style.backgroundSize = "300%"
+    console.log('Right');
+  
+  } else if (event.target.attributes[1].value === 'in') {
+    zoom += 20;
+    inspector.style.backgroundSize = zoom + "%"
+    console.log('Zoom-in:'+ inspector.style.backgroundSize);
+  
   } else if (event.target.attributes[1].value === 'out') {
-    console.log('Zoom-out');
-    inspector.style.backgroundSize = "100%"
+    zoom -= 20;
+    inspector.style.backgroundSize = zoom + "%"
+    console.log('Zoom-out:'+ inspector.style.backgroundSize);
   }
 });
