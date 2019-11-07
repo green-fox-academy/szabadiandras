2ed64636ba15945393e92216be2f55a0cf6e96e6
szabadiandras <szabadiandras93@gmail.com>
2019-11-07 16:00:41 +0100

    1-6 finished

---
+++ week-08/day-04/promise/06.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let promise = new Promise(function(fulfill, rejected) {
+  fulfill("FULFILLED");
+  rejected(new Error("ERROR"));
+});
+
+promise.catch(function(error) {
+  console.error("ERROR");
+});
+
+Promise.resolve("PROMISE.RESOLVE");
+
+Promise.reject(new Error("PROMIE.REJECT"));
