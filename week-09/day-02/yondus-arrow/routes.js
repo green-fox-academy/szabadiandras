6de590d5e9e8797c5e9a02aaf3a8fd2685b03d6f
szabadiandras <szabadiandras93@gmail.com>
2019-11-12 16:56:14 +0100

    testings added

--- week-09/day-02/yondus-arrow/routes.js
+++ week-09/day-02/yondus-arrow/routes.js
@@ -4,12 +4,13 @@ const app = express();
 app.get("/yondu", (req, res) => {
   let distance = req.query.distance;
   let time = req.query.time;
+  let speed = distance / time;
   if (distance && time) {
     res.status(200);
     res.json({
       distance: distance,
       time: time,
-      speed: distance / time
+      speed: speed.toString()
     });
   } else {
     res.status(404);
--- week-09/day-02/yondus-arrow/test/routes.js
+++ week-09/day-02/yondus-arrow/test/routes.js
@@ -7,22 +7,14 @@ const app = require("../routes");
 test("yondu endpoint", t => {
   request(app)
     .get("/yondu")
-    .query({
-      distance: "1500",
-      time: "10",
-      speed: "150"
-    })
+    .query({ distance: "1500", time: "10", speed: "150" })
     .expect("Content-Type", /json/)
     .expect(200)
     .end(function(err, res) {
       t.error(err, "No error");
       t.deepEqual(
         res.body,
-        {
-          distance: "1500",
-          time: "10",
-          speed: "150"
-        },
+        { distance: "1500", time: "10", speed: "150" },
         "Numbers added"
       );
       t.end();
@@ -38,9 +30,7 @@ test("yondu endpoint", t => {
       t.error(err, "No error");
       t.deepEqual(
         res.body,
-        {
-          error: "Invalid input"
-        },
+        { error: "Invalid input" },
         "Task failed succesfully"
       );
       t.end();
