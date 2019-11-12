1816610a7f6e0468d57bda09e3be60d6ac6dc4dc
szabadiandras <szabadiandras93@gmail.com>
2019-11-12 15:37:18 +0100

    testing

--- week-09/day-02/iamgroot/routes.js
+++ week-09/day-02/iamgroot/routes.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/groot', (req, res) => {
-  // TODO: implement this method
+app.get("/groot", (req, res) => {
+  let message = req.query.message;
+  if (message) {
+    res.status(200);
+    res.json({
+      received: message,
+      translated: "I am Groot!"
+    });
+  } else {
+    res.status(404);
+    res.json({ error: "I am Groot!" });
+  }
 });
 
 module.exports = app;
