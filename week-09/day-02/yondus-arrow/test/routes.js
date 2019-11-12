7836ec8949d5cbe9a72e3021f3ee298cfe582bb0
szabadiandras <szabadiandras93@gmail.com>
2019-11-12 15:59:10 +0100

    yonda

---
+++ week-09/day-02/yondus-arrow/test/routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const app = require('../routes');
+​
+test('groot endpoint', (t) => {
+  // TODO: implement it
+});
