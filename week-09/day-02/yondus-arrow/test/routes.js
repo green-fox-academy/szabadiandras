fe1ee373bbc00f8d0699bfe106ee65f793f0753a
szabadiandras <szabadiandras93@gmail.com>
2019-11-12 16:36:01 +0100

    testing added

--- week-09/day-02/yondus-arrow/test/routes.js
+++ week-09/day-02/yondus-arrow/test/routes.js
@@ -4,30 +4,45 @@ const test = require("tape");
 const request = require("supertest");
 const app = require("../routes");
 
-test("groot endpoint", t => {
+test("yondu endpoint", t => {
   request(app)
-    .get("/groot")
-    .query({message : 'somemessage' })
+    .get("/yondu")
+    .query({
+      distance: "1500",
+      time: "10",
+      speed: "150"
+    })
     .expect("Content-Type", /json/)
     .expect(200)
     .end(function(err, res) {
       t.error(err, "No error");
-      t.deepEqual(res.body, { 
-        received: 'somemessage',
-        translated: "I am Groot!"
-       }, "Message not added");
+      t.deepEqual(
+        res.body,
+        {
+          distance: "1500",
+          time: "10",
+          speed: "150"
+        },
+        "Numbers added"
+      );
       t.end();
     });
 });
 
-test("groot endpoint", t => {
+test("yondu endpoint", t => {
   request(app)
-    .get("/groot")
+    .get("/yondu")
     .expect("Content-Type", /json/)
     .expect(404)
     .end(function(err, res) {
       t.error(err, "No error");
-      t.deepEqual(res.body, { error: "I am Groot!" }, "Message not added");
+      t.deepEqual(
+        res.body,
+        {
+          error: "Invalid input"
+        },
+        "Task failed succesfully"
+      );
       t.end();
     });
 });
