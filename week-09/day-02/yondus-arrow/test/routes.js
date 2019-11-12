211b1f9e82e9d02f1173f664ed4fddd112ba0693
szabadiandras <szabadiandras93@gmail.com>
2019-11-12 16:03:58 +0100

    yonda's

--- week-09/day-02/yondus-arrow/test/routes.js
+++ week-09/day-02/yondus-arrow/test/routes.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
-const test = require('tape');
-const request = require('supertest');
-const app = require('../routes');
-​
-test('groot endpoint', (t) => {
-  // TODO: implement it
+const test = require("tape");
+const request = require("supertest");
+const app = require("../routes");
+
+test("groot endpoint", t => {
+  request(app)
+    .get("/groot")
+    .query({ received: "message" })
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .end(function(err, res) {
+      t.error(err, "No error");
+      t.deepEqual(res.body, { error: "Error" }, "Invalid input");
+      t.end();
+    });
 });
