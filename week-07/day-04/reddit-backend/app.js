5f3b6d52b1cd52c77143bd7b637eca4ff0da20e3
szabadiandras <szabadiandras93@gmail.com>
2019-11-14 09:57:55 +0100

    cleaning

--- week-07/day-04/reddit-backend/app.js
+++ week-07/day-04/reddit-backend/app.js
@@ -1,139 +1,142 @@
-const mysql = require('mysql');
-const express = require('express');
-const path = require('path');
+const mysql = require("mysql");
+const express = require("express");
+const path = require("path");
 const PORT = 8080;
 
 let conn = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'greenfox',
-  database: 'reddit',
+  host: "127.0.0.1",
+  user: "root",
+  password: "greenfox",
+  database: "reddit"
 });
 
-conn.connect(function (err) {
+conn.connect(function(err) {
   if (err) {
-    console.log('Error connecting to Db');
+    console.log("Error connecting to Db");
     console.log(err);
     return;
   }
-  console.log('Connection to database established\n');
+  console.log("Connection to database established\n");
 });
 
 let app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-  console.log('Client request: "Main page".')
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+  console.log('Client request: "Main page".');
 });
 
-app.get('/hello', function (req, res) {
-  console.log("hello world")
-  res.send('Hello');
+app.get("/hello", function(req, res) {
+  console.log("hello world");
+  res.send("Hello");
 });
-
 
 //GET POSTS
 
-app.get('/posts', (req, res) => {
-  const query = "SELECT * FROM post"
+app.get("/posts", (req, res) => {
+  const query = "SELECT * FROM post";
   conn.query(query, (err, posts) => {
     res.setHeader("Content-type", "application/json");
     res.status(200);
     res.send(JSON.stringify(posts));
-    console.log('Client request: "Get API".')
+    console.log('Client request: "Get API".');
   });
 });
-
 
 // GET USERS
 
-app.get('/owner', (req, res) => {
-  const query = "SELECT * FROM reddit.owner"
+app.get("/owner", (req, res) => {
+  const query = "SELECT * FROM reddit.owner";
   conn.query(query, (err, owner) => {
     res.status(200);
     res.send(JSON.stringify(owner));
-    console.log('Client request: "Get Owner database".')
+    console.log('Client request: "Get Owner database".');
   });
 });
 
-
 //GET POST DATABASE
 
-app.get('/posts/db', (req, res) => {
-  const query = "SELECT * FROM post"
+app.get("/posts/db", (req, res) => {
+  const query = "SELECT * FROM post";
   conn.query(query, (err, posts) => {
     res.setHeader("Content-type", "application/json");
     res.status(200);
     res.send(JSON.stringify(posts));
-    console.log('Client request: "Get Post database".')
+    console.log('Client request: "Get Post database".');
   });
 });
-
 
 //NEW POST
 
-app.post('/posts', function (req, res) {
+app.post("/posts", function(req, res) {
   const dateObj = new Date();
-  const date = `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
-  const query = `INSERT INTO reddit.post (title, url, timestamp, score) VALUES ('${req.body.title}', 'localhost:8080/posts/${req.body.title}', '${date}', '0')`
+  const date = `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
+  const query = `INSERT INTO reddit.post (title, url, timestamp, score) VALUES ('${req.body.title}', 'localhost:8080/posts/${req.body.title}', '${date}', '0')`;
   conn.query(query, (err, post) => {
-    console.log(err)
+    console.log(err);
     console.log(post);
-    const query = `SELECT * FROM post WHERE id=${post.insertId}`
+    const query = `SELECT * FROM post WHERE id=${post.insertId}`;
     conn.query(query, (err, post) => {
       res.setHeader("Content-type", "application/json");
       res.status(200);
-      res.send(JSON.stringify(post) + `\n"${req.body.title}" is added to posts.`);
-    })
+      res.send(
+        JSON.stringify(post) + `\n"${req.body.title}" is added to posts.`
+      );
+    });
   });
 });
-
 
 //UPVOTE
 
-app.put('/posts/:id/upvote', function (req, res) {
-  const query = `UPDATE reddit.post SET score = score + 1 WHERE (id = ${req.params.id})`
+app.put("/posts/:id/upvote", function(req, res) {
+  const query = `UPDATE reddit.post SET score = score + 1 WHERE (id = ${req.params.id})`;
   conn.query(query, (err, posts) => {
-    console.log('Checking for errors: ' + err)
+    console.log("Checking for errors: " + err);
     res.setHeader("Content-type", "application/json");
     res.status(200);
-    res.send(JSON.stringify(query) + `\nThe post "${req.body.title}" has been upvoted.`);
-    console.log(`Client request: "Upvote post called '${req.body.title}' in database".`)
+    res.send(
+      JSON.stringify(query) + `\nThe post "${req.body.title}" has been upvoted.`
+    );
+    console.log(
+      `Client request: "Upvote post called '${req.body.title}' in database".`
+    );
   });
 });
-
 
 //DOWNVOTE
 
-app.put('/posts/:id/downvote', function (req, res) {
-  const query = `UPDATE reddit.post SET score = score - 1 WHERE (id = ${req.params.id})`
+app.put("/posts/:id/downvote", function(req, res) {
+  const query = `UPDATE reddit.post SET score = score - 1 WHERE (id = ${req.params.id})`;
   conn.query(query, (err, posts) => {
-    console.log('Checking for errors: ' + err)
+    console.log("Checking for errors: " + err);
     res.setHeader("Content-type", "application/json");
     res.status(200);
-    res.send(JSON.stringify(query) + `\nThe post "${req.body.title}" has been downvoted. Current score is ${req.body.score}`);
-    console.log(`Client request: "Downvote post called '${req.body.title}' in database".`)
+    res.send(
+      JSON.stringify(query) +
+        `\nThe post "${req.body.title}" has been downvoted. Current score is ${req.body.score}`
+    );
+    console.log(
+      `Client request: "Downvote post called '${req.body.title}' in database".`
+    );
   });
 });
-
 
 //DELETE (optional)
 
-app.delete('/posts/:id', function (req, res) {
-  const query = `DELETE FROM reddit.post WHERE id = ${req.params.id}`
+app.delete("/posts/:id", function(req, res) {
+  const query = `DELETE FROM reddit.post WHERE id = ${req.params.id}`;
   conn.query(query, (err, posts) => {
     res.setHeader("Content-type", "application/json");
     res.status(200);
-    res.send(JSON.stringify(query) + '\n' + req.params.id + ' is deleted.');
+    res.send(JSON.stringify(query) + "\n" + req.params.id + " is deleted.");
   });
 });
 
-
 //PUT (optional)
 
-app.put('/posts/:id', function (req, res) {
+app.put("/posts/:id", function(req, res) {
   res.send(req.params.id);
 });
 
